934545571b566eec5fd9c3f06592ea9b661f5245
christian <cpietra@clinicaconstituyentes.com>
2019-10-10 15:17:02 -0300

    master

--- Estadisticas/Program.cs
+++ Estadisticas/Program.cs
@@ -1,17 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Estadisticas
 {
